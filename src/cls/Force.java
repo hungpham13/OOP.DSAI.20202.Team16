86050a82204ceec7a7cd2634d7588900aed5306e
calibretaliation <nhat.hnm194445@sis.hust.edu.vn>
2021-05-25 17:19:57 +0700

    Force begin

---
+++ src/cls/Force.java
@@ -0,0 +1,22 @@
+package cls;
+
+public class Force {
+    private float value;
+    //Constructor
+    public Force(float value){
+        this.value = value;
+    }
+    public Force() {
+    }
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+    public Force plus(Force other) {
+        return new Force(this.value + other.getValue());
+    }
+
+}
--- src/cls/Monitor.java
+++ src/cls/Monitor.java
@@ -4,7 +4,7 @@ public class Monitor {
     private Object obj = null;
     private boolean playing;
     private Surface surface;
-    //private Force actor;
+    private Force actor;
 
     public boolean isEmpty(){
         return obj == null;
@@ -12,9 +12,24 @@ public class Monitor {
     public void setObj(Object newObj){
         obj = newObj;
     }
-    //public Force getFrictionForce(){
-        //todo
-    //}
+    public Force getFrictionForce(){
+        Force frictionForce = new Force();
+        float normalForce = 10*obj.getMass();
+        if ((Cube) obj instanceof Cube ) {
+            if (actor.getValue() <= (normalForce*surface.getStaticFrictionCoef())) {
+                frictionForce.setValue(actor.getValue());
+            } else {
+                frictionForce.setValue(normalForce*surface.getKineticFrictionCoef);
+            }
+        } else if ((Cylinder) obj instanceof Cylinder) {
+            if (actor.getValue() <= (3*normalForce*surface.getStaticFrictionCoef())) {
+                frictionForce.setValue(actor.getValue()/3);
+            } else {
+                frictionForce.setValue(normalForce*surface.getKineticFrictionCoef());
+            }
+        }
+        return frictionForce;
+    }
     public void pause(){
         //todo
     }
